de59845595c902e203d2c83e320b4471e337bcc0
Rémy Greinhofer <remy.greinhofer@gmail.com>
2019-03-24 13:35:56 -0500

    Use pycalver for tagging
    Adds pycalver commands to the update script to be able to automatically
    tag the repository when new data is added.
    
    Fixes scrapd/datasets#9

--- .circleci/update-datasets.sh
+++ .circleci/update-datasets.sh
@@ -22,7 +22,9 @@ if [ -n "${HAS_CHANGE}" ]; then
   git commit -am "Update data sets" \
     -m "There are ${NEW_ENTRY_COUNT} new entries in the current data set." \
     -m "${HAS_CHANGE}"
+  pycalver bump --patch
   git push origin master
+  git push --tags
 else
   echo "There is nothing new to commit."
 fi
