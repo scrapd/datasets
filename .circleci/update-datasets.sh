9cc1cc1c0f210af9167dab0d338b0f250fcf8cbe
Rémy Greinhofer <remy.greinhofer@gmail.com>
2019-04-29 17:44:03 -0500

    Update pipeline logic (#32)
    Updates the pipeline logic to reflect the steps defined in the README
    file.
    
    Fixes scrapd/datasets#31

--- .circleci/update-datasets.sh
+++ .circleci/update-datasets.sh
@@ -1,37 +1,55 @@
 #!/bin/bash
 set -xeuo pipefail
 
+# Define variables.
 TOPDIR=$(git rev-parse --show-toplevel)
 CURRENT_YEAR=$(date +%Y)
 CURRENT_DATASET="fatalities-${CURRENT_YEAR}-raw.json"
 
+# Generate the current data set.
 cd "${TOPDIR}/datasets"|| exit
 ENTRY_COUNT_BEFORE=$(jq length "${CURRENT_DATASET}")
 python "${TOPDIR}/tools/scrapd-merger.py" -i "${CURRENT_DATASET}" <(scrapd -v --format json --from "Jan 1 ${CURRENT_YEAR}" --to "Dec 31 ${CURRENT_YEAR}");
 HAS_CHANGE=$(git status -s)
-if [ -n "${HAS_CHANGE}" ]; then
-  # Update the `-all` data set.
-  jq -s add fatalities-20*-raw.json > fatalities-all-raw.json
 
-  # Augment the data sets.
-  python "${TOPDIR}/tools/scrapd-augmenter-geocoding-geocensus.py" fatalities-all-raw.json > fatalities-all-augmented.json
-
-  # Compute the number of new entries.
-  ENTRY_COUNT_AFTER=$(jq length "${CURRENT_DATASET}")
-  NEW_ENTRY_COUNT=$(( ENTRY_COUNT_AFTER - ENTRY_COUNT_BEFORE ))
-  echo "There are ${NEW_ENTRY_COUNT} new entries in the current data set."
-
-  # Go back to the top dir.
-  cd "${TOPDIR}"|| exit
-
-  # Commit the changes.
-  git add .
-  git commit -m "Update data sets" \
-    -m "There are ${NEW_ENTRY_COUNT} new entries in the current data set." \
-    -m "${HAS_CHANGE}"
-  pycalver bump -n || pycalver bump -n --patch
-  git push origin master
-  git push --tags
-else
+# If nothing changed, we can leave.
+if [ -z "${HAS_CHANGE}" ]; then
   echo "There is nothing new to commit."
+  exit 0
 fi
+
+# Import external data sets.
+for YEAR in {17..18}; do
+  # Create empty sets if they don't exist yet.
+  [ ! -f "fatalities-20${YEAR}-augmented.json" ] && echo "[]" > "fatalities-20${YEAR}-augmented.json"
+
+  # Import data from Socrata.
+  #python "${TOPDIR}/tools/scrapd-importer-fatalities-socrata.py" "fatalities-20${YEAR}-raw.json" "${TOPDIR}/external-datasets/socrata-apd-archives/socrata-apd-20${YEAR}.json" > "fatalities-20${YEAR}-augmented.json"
+done
+
+# Augment the current data set.
+for f in fatalities-20{17..19}-augmented.json; do
+  # Augment the data with geocoding information from geocensus.
+  [ -f "${f}"] && python "${TOPDIR}/tools/scrapd-augmenter-geocoding-geocensus.py"-i ${f};
+done
+
+# Merge the results.
+jq -s add fatalities-20{17..19}-raw.json > fatalities-all-raw.json
+jq -s add fatalities-20{17..19}-augmented.json > fatalities-all-raw.json
+
+# Compute the number of new entries.
+ENTRY_COUNT_AFTER=$(jq length "${CURRENT_DATASET}")
+NEW_ENTRY_COUNT=$(( ENTRY_COUNT_AFTER - ENTRY_COUNT_BEFORE ))
+echo "There are ${NEW_ENTRY_COUNT} new entries in the current data set."
+
+# Go back to the top dir.
+cd "${TOPDIR}"|| exit
+
+# Commit the changes.
+git add .
+git commit -m "Update data sets" \
+  -m "There are ${NEW_ENTRY_COUNT} new entries in the current data set." \
+  -m "${HAS_CHANGE}"
+pycalver bump -n || pycalver bump -n --patch
+git push origin master
+git push --tags
