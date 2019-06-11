6c73067c0562d410cbaba30da630c7392450a4c7
Rémy Greinhofer <remy.greinhofer@gmail.com>
2019-06-11 18:29:54 -0500

    Update data sets with ScrAPD 2.0.0 (#50)
    * Add tools to update raw data sets.
    * Update raw data sets with the new tools.
    * Update dependencies.

---
+++ tools/update-raw-data-sets.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+# Define variables.
+TOPDIR=$(git rev-parse --show-toplevel)
+DATASET_DIR="${TOPDIR}/datasets"
+TOOL_DIR="${TOPDIR}/tools"
+MERGER="${TOPDIR}/tools/scrapd-merger.py"
+
+# Ensure we are in the top directory.
+cd "${TOPDIR}"|| exit
+
+# Generate the current data set.
+for YEAR in {2017..2019}; do
+  DATASET="${DATASET_DIR}/fatalities-${YEAR}-raw.json"
+  python "${MERGER}" -i -u "${DATASET}" <(scrapd -v --format json --from "Jan 1 ${YEAR}" --to "Dec 31 ${YEAR}");
+done
