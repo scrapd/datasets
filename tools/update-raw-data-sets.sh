8ee5ef9053dc3c4103f141d657c843d2a5fa1329
Rémy Greinhofer <remy.greinhofer@gmail.com>
2019-07-17 19:39:51 -0500

    Update to ScrAPD 2.1.0 (#56)
    * Update to ScrAPD 2.1.0
    
    Updates the pipeline to use ScrAPD 2.1.0.
    
    Drive-bys:
    * Update Raw data sets
    * Update Changelog

--- tools/update-raw-data-sets.sh
+++ tools/update-raw-data-sets.sh
@@ -12,6 +12,7 @@ cd "${TOPDIR}"|| exit
 
 # Generate the current data set.
 for YEAR in {2017..2019}; do
+  echo "=> Processing year ${YEAR}..."
   DATASET="${DATASET_DIR}/fatalities-${YEAR}-raw.json"
   python "${MERGER}" -i -u "${DATASET}" <(scrapd -v --format json --from "Jan 1 ${YEAR}" --to "Dec 31 ${YEAR}");
 done
